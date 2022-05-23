115ed2f89cd17c9eb76e8d0589aa028880160d21
jaywerra <jay@jaywerra.com>
2022-05-23 16:05:28 -0700

    Tailwind installation

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,8 +1,10 @@
 import * as React from "react";
  
 const Feature = ({ blok }) => (
-    <div className="feature">
-        <h2>{blok.name}</h2>
+    <div className="feature rounded border-blue-500 border-opacity-100 border p-8">
+        <h2 className="font-bold text-xl mb-2">
+            {blok.name}
+        </h2>
     </div>
 )
  
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import DynamicComponent from "./dynamicComponent";
 
 const Grid = ({ blok }) => (
-    <div className="grid">
+    <div className="grid grid-cols-3">
         {blok.columns.map(blok => {
             return (
                 <DynamicComponent blok={blok} />
