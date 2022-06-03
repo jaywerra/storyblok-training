554315c0f68d81bd18a5c9c58f4d96b5a07e4866
Jay <jaywerra@gmail.com>
2022-06-03 11:41:46 -0700

    Temp disabling g-node and template files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,33 +1,33 @@
-const path = require('path')
+// const path = require('path')
 
-exports.createPages = async ({ graphql, actions }) => {
-  const storyblokEntry = path.resolve('src/templates/storyblok-entry.js')
+// exports.createPages = async ({ graphql, actions }) => {
+//   const storyblokEntry = path.resolve('src/templates/storyblok-entry.js')
 
-  // querying the storyblok data from GraphQL data layer
-  const { data } = await graphql(
-    `query {
-      allStoryblokEntry {
-        edges {
-          node {
-            id
-            full_slug
-          }
-        }
-      }
-    }`
-  )
+//   // querying the storyblok data from GraphQL data layer
+//   const { data } = await graphql(
+//     `query {
+//       allStoryblokEntry {
+//         edges {
+//           node {
+//             id
+//             full_slug
+//           }
+//         }
+//       }
+//     }`
+//   )
 
-  // creating pages using createPage function like described in the documentation
-  // https://www.gatsbyjs.org/docs/programmatically-create-pages-from-data/#creating-pages
-  data.allStoryblokEntry.edges.forEach(edge => {
-    const full_slug = edge.node.full_slug
+//   // creating pages using createPage function like described in the documentation
+//   // https://www.gatsbyjs.org/docs/programmatically-create-pages-from-data/#creating-pages
+//   data.allStoryblokEntry.edges.forEach(edge => {
+//     const full_slug = edge.node.full_slug
 
-    actions.createPage({
-      path: full_slug,
-      component: storyblokEntry,
-      context: {
-        slug: full_slug
-      },
-    })
-  })
-}
+//     actions.createPage({
+//       path: full_slug,
+//       component: storyblokEntry,
+//       context: {
+//         slug: full_slug
+//       },
+//     })
+//   })
+// }
