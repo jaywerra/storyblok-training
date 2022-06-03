419cdd3cb4e3bcfe11da9529cb629e81ba9f9d20
Jay <jaywerra@gmail.com>
2022-06-03 11:15:58 -0700

    Node and template file

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,33 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+const path = require('path')
+
+exports.createPages = async ({ graphql, actions }) => {
+  const storyblokEntry = path.resolve('src/templates/storyblok-entry.js')
+
+  // querying the storyblok data from GraphQL data layer
+  const { data } = await graphql(
+    `query {
+      allStoryblokEntry {
+        edges {
+          node {
+            id
+            full_slug
+          }
+        }
+      }
+    }`
+  )
+
+  // creating pages using createPage function like described in the documentation
+  // https://www.gatsbyjs.org/docs/programmatically-create-pages-from-data/#creating-pages
+  data.allStoryblokEntry.edges.forEach(edge => {
+    const full_slug = edge.node.full_slug
+
+    actions.createPage({
+      path: full_slug,
+      component: storyblokEntry,
+      context: {
+        slug: full_slug
+      },
+    })
   })
 }
