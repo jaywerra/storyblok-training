9776bed5545536f25ad57c4d380ca8938c042952
Jay <jaywerra@gmail.com>
2022-06-08 14:42:27 -0700

    Init nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,11 +4,19 @@ import React from 'react'
 const Nav = () => {
     const links = [
         {
-            linkLabel: "Features",
-            linkHref: "/features/",
+            linkLabel: "Product",
+            linkHref: "/",
         },
         {
-            linkLabel: "Contact",
+            linkLabel: "Company",
+            linkHref: "/",
+        },
+        {
+            linkLabel: "Case Studies",
+            linkHref: "/",
+        },
+        {
+            linkLabel: "Contact us",
             linkHref: "/contact/"
         },
     ]
@@ -17,7 +25,10 @@ const Nav = () => {
            <ul className="flex">
                {links.map(link => {
                    return (
-                        <li key={link.linkLabel} className="ml-10">
+                        <li 
+                            key={link.linkLabel} 
+                            className="ml-10"
+                        >
                             <Link
                                 to={link.linkHref}
                                 className="block py-1 font-mono border-b border-white border-solid hover:border-black ease-linear uppercase"
@@ -32,4 +43,18 @@ const Nav = () => {
     )
 }
 
-export default Nav
+export default Nav;
+
+/*
+    Main Nav - nav
+    - Parent Links - ul > li > a
+        - Sub Menu - child div
+            - Three Columns 
+            - Parent Links
+                - Two Columns
+                    - Left Column
+                        - Menu / Nav
+                        - Promotional content, e.g., Image, Button, Headline
+                    - Right Nav
+                         - Promotional content, e.g., Image, Button, Headline
+*/
