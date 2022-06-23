03b80032f61c20bcf806bbfffae42125a409dc0a
Jay <jaywerra@gmail.com>
2022-06-23 15:39:00 -0700

    Markup cleanup

--- src/components/NavMain/NavMain.js
+++ src/components/NavMain/NavMain.js
@@ -5,6 +5,7 @@ import NavTabs from '../NavTabs/NavTabs';
 
 const NavMain = ({ blok }) => {
     const [subnav, setSubnav] = useState(null);
+    const [showMenu, setShowMenu] = useState(false);
     const ref = useRef();
 
     useOnClickOutside(ref, () => setSubnav(false));
@@ -15,12 +16,22 @@ const NavMain = ({ blok }) => {
 		}
 		setSubnav(i);
     }
-    // console.log(blok)
+
+    const burgerClick = () => {
+        setShowMenu(!showMenu);
+    }
+
 	return (
         <div className="relative px-4 lg:px-0 lg:container lg:mx-auto z-10">
             <div className="px-4 lg:px-0 lg:container lg:mx-auto py-4 mt-12">
+                <button
+                    onClick={burgerClick}
+                    className="md:hidden py-2 px-6 border border-solid border-blue-700"
+                >
+                    Menu
+                </button>
                 <nav
-                    className={`md:block absolute md:static left-0 top-16 md:top-0 w-screen md:w-auto bg-white h-auto md:h-auto py-4 md:py-0`}
+                    className={`${showMenu ? 'block' : 'hidden'} md:block absolute md:static left-0 top-16 md:top-0 w-screen md:w-auto bg-white h-auto md:h-auto py-4 md:py-0`}
                     ref={ref}
                 >
                     <ul className="md:flex uppercase tracking-widest">
@@ -34,7 +45,6 @@ const NavMain = ({ blok }) => {
                                         Check for dropdown - if it has a dropdown render button 
                                         with dropdown markup otherwise render as link 
                                     */}
-                                    {console.log("Item", item)}
                                     {item?.tab_content ?
                                         <>
                                             <button
@@ -50,21 +60,23 @@ const NavMain = ({ blok }) => {
                                                 {item?.tab_content && (
                                                     <NavTabs navHeadingTitle={item?.section_title}>
                                                         {item?.tab_content?.map(linkEl => {
-                                                            console.log("Link El", linkEl?.nav_cards?.card_title)
                                                             return (
                                                                 <div
                                                                     key={linkEl?.link_label} 
                                                                     label={linkEl?.link_label}
                                                                     pageLink={linkEl?.link_url?.cached_url}
                                                                     content={linkEl?.nav_card?.map(card => (
-                                                                        <div className="w-1/3 mx-2">
+                                                                        <div className="w-1/3 mr-10 last:mr-0">
                                                                             <div className="mb-4">
-                                                                                <img src={card.thumbnail.filename} alt="" />
+                                                                                <Link to={card.card_link}>
+                                                                                    <img src={card.thumbnail.filename} alt="" />
+                                                                                </Link>
                                                                             </div>
                                                                             <h2 className="text-center">
-                                                                                {card.card_title}
+                                                                                <Link to={card.card_link}>
+                                                                                    {card.card_title}
+                                                                                </Link>
                                                                             </h2>
-                                                                            {/* <div dangerouslySetInnerHTML={{__html: card?.card_summary?.content}} /> */}
                                                                         </div>
                                                                     ))}
                                                                 />
--- src/components/NavTabs/NavTabs.js
+++ src/components/NavTabs/NavTabs.js
@@ -21,13 +21,12 @@ const NavTabs = ({
         <div
             className="md:flex w-full p-4 bg-slate-300 md:bg-white"
         >
-            <div className="tabsctas md:pr-8 md:w-1/4 md:border-r border-solid border-slate-200">
+            <div className="tabs-controls md:pr-8 md:w-1/4 md:border-r border-solid border-slate-200">
                 <h2 className="bg-blue-100 text-blue-500 inline-block mb-6 p-3">
                     {navHeadingTitle}
                 </h2>
                 <ul>
                     {children.map(tab => {
-                        {console.log('Tab Link', tab.props.pageLink)}
                         if (!tab.props.pageLink) {
                             return (
                                 <li
@@ -59,23 +58,31 @@ const NavTabs = ({
                     })}
                 </ul>
             </div>
-            <div className="panels md:w-3/4 md:pl-8">
+            <>
                 {children.map(one => {
                     if (one.props.label === activeTab && one.props.pageLink.length === 0) {
                         return (
-                            <div
+                            <motion.div
                                 key={one.props.label}
-                                className="tab-panel"
+                                className="tab-panel flex px-8"
+                                initial={{
+                                    opacity: 0,
+                                }}
+                                animate={{
+                                    opacity: 1,
+                                }}
+                                transition={{ 
+                                    ease: "easeInOut", 
+                                    duration: 0.35 
+                                }}
                             >
-                                <div className="flex">
-                                    {one.props.content}
-                                </div>
-                            </div>
+                                {one.props.content}
+                            </motion.div>
                         );
                     }
                     return null;
                 })}
-            </div>
+            </>
         </div>
     )
 }
